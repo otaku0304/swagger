7c50462952163187d111e433308459814482da4e
SaiKrishnaAnnam <sai.annam@betteranalytics.in>
2024-02-04 10:14:28 +0530

    added private constructor for converter

--- src/main/java/swagger/converter/SwaggerConverter.java
+++ src/main/java/swagger/converter/SwaggerConverter.java
@@ -6,15 +6,13 @@ import swagger.entity.Swagger;
 public class SwaggerConverter {
 
     private SwaggerConverter() {
-        throw new IllegalStateException("SwagConverter class");
+        throw new IllegalStateException("SwaggerConverter class");
     }
 
     public static Swagger convertSwagDTOtoEntity(final SwaggerDTO swagDTO) {
         return Swagger.builder()
                 .swagContent(swagDTO.getSwagContent())
                 .user(swagDTO.getUser())
-                //.genericDetails(utility.createGenericDetails(swagDTO.getUser()))
                 .build();
-
     }
 }
